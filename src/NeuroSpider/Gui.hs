440a17ebdee10221413b47be3e76f8e7367f6ece
Pavel Kogan <pkogan@gmail.com>
2014-07-16 02:30:42 +0100

    Move svg transform code from Gui to XML source file

--- src/NeuroSpider/Gui.hs
+++ src/NeuroSpider/Gui.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 
 module NeuroSpider.Gui where
 
@@ -14,9 +13,9 @@ import Data.Graph.Inductive.Example (vor)
 import Data.Graph.Inductive.Graph (nodeRange, insNode)
 --import Data.String
 
-import Prelude hiding (lookup, mapM)
+import Prelude hiding (mapM)
 import Data.Default
-import Data.Map (Map, (!), fromList, lookup, insert, elems)
+import Data.Map (Map, (!), fromList, elems)
 import Data.Text.Lazy (Text, unpack)
 import Data.Traversable (mapM)
 import Graphics.UI.Gtk
@@ -102,24 +101,3 @@ runGUI = doGUI $ withBuilder "main.glade" $ \builder -> do
     js <- T.readFile =<< getDataFileName "main.js"
     let svg = renderText def $ transformSvg (parseText_ def xml) css js
     webViewLoadString wv (unpack svg) (Just "image/svg+xml") Nothing ""
-
-transformSvg :: Document -> Text -> Text -> Document
-transformSvg d@(Document{..}) css js = d{documentRoot = documentRoot'}
-  where
-    documentRoot' = goElem documentRoot
-    goElem e@(Element{..}) = case elementName of
-      Name{nameLocalName = "svg",..} -> e{elementNodes = script++style++nodes}
-      Name{nameLocalName = "g"  ,..} ->
-        let newAttrs = addClick elementAttributes
-        in  e{elementNodes = nodes, elementAttributes = newAttrs}
-      _                              -> e{elementNodes = nodes}
-      where nodes  = fmap goNode elementNodes
-            style  = svgNodes "style"  css
-            script = svgNodes "script" js
-    goNode (NodeElement e) = NodeElement $ goElem e
-    goNode n               = n
-    addClick attrs = case lookup "class" attrs of
-      Just a  -> if a `elem` ["node", "edge"]
-                   then insert "onclick" "clickHandler(this)" attrs
-                   else attrs
-      Nothing -> attrs
--- src/NeuroSpider/Util/XML.hs
+++ src/NeuroSpider/Util/XML.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module NeuroSpider.Util.XML where
 
+import Prelude hiding (lookup)
+import Data.Map (lookup, insert)
 import Data.Text.Lazy
 import Data.String
 import Text.XML
@@ -20,3 +23,23 @@ svgNodes tag = elem' (name tag) attrs
 elem' :: Name -> [(Name, S.Text)] -> Text -> [Node]
 elem' t as = render . elementA t as . content . toStrict
 
+transformSvg :: Document -> Text -> Text -> Document
+transformSvg d@(Document{..}) css js = d{documentRoot = documentRoot'}
+  where
+    documentRoot' = goElem documentRoot
+    goElem e@(Element{..}) = case elementName of
+      Name{nameLocalName = "svg",..} -> e{elementNodes = script++style++nodes}
+      Name{nameLocalName = "g"  ,..} ->
+        let newAttrs = addClick elementAttributes
+        in  e{elementNodes = nodes, elementAttributes = newAttrs}
+      _                              -> e{elementNodes = nodes}
+      where nodes  = fmap goNode elementNodes
+            style  = svgNodes "style"  css
+            script = svgNodes "script" js
+    goNode (NodeElement e) = NodeElement $ goElem e
+    goNode n               = n
+    addClick attrs = case lookup "class" attrs of
+      Just a  -> if a `elem` ["node", "edge"]
+                   then insert "onclick" "clickHandler(this)" attrs
+                   else attrs
+      Nothing -> attrs
